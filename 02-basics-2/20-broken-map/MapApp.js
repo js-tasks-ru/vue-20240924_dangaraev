3bdba9b0ca3957c6329163acecd219e78efd0b4e
d.garaev <garaevdanil@yandex.ru>
2024-09-28 21:28:48 +0500

    Broken map task

---
+++ 02-basics-2/20-broken-map/MapApp.js
@@ -0,0 +1,41 @@
+import { defineComponent, ref, watch } from 'vue'
+
+export default defineComponent({
+  name: 'MapApp',
+
+  setup() {
+    // Реактивные переменные для хранения координат метки
+    const x = ref(0)
+    const y = ref(0)
+
+    const pin = ref()
+
+    /**
+     * Обработчик клика по карте для установки координат метки
+     * @param {MouseEvent} event
+     */
+    function handleClick(event) {
+      x.value = event.offsetX
+      y.value = event.offsetY
+    }
+
+    // Следим за X и Y для установки нового положения
+    watch(() => [x.value, y.value], () => {
+      // Находим метку и изменяем её положение
+      pin.value.style.left = `${x.value}px`
+      pin.value.style.top = `${y.value}px`
+    })
+
+    return {
+      handleClick,
+      pin
+    }
+  },
+
+  template: `
+    <div class="map" @click="handleClick">
+      <img class="map-image" src="./map.png" alt="Map" draggable="false" />
+      <span ref="pin">📍</span>
+    </div>
+  `,
+})
